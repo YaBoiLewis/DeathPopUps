00c4fcf2e1bf9901baab6996537cfc9934d0be0b
SavionLegendZzz <saviondeavault@gmail.com>
2015-10-01 15:36:06 -0400

    Create Main.php

---
+++ src/DP/Main.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace DP;
+
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByChildEntityEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+
+class Main extends PluginBase implements Listener{
+
+public function onEnable(){
+
+$this->getServer()->getLogger()->info(TextFormat::BLUE."[DeathPopups]Plugin Enabled!");
+$this->getServer()->getPluginManager()->registerEvents($this,$this);
+}
+
+public function onDeath(PlayerDeathEvent $ev){
+$p=$ev->getEntity();
+if($ev instance of EntityDamageByEntityEvent){
+$killer = $ev->getEntity()->getLastDamageCause()->getDamager();
+if($ev instanceof Player){
+$killer->sendPopup("You Killed ".$p->getName()."!!!!");
+}
+}
+if($ev instance of EntityDamageByChildEntityEvent){
+$killer = $ev->getEntity()->getLastDamageCause()->getDamager();
+if($ev instanceof Player){
+$killer->sendPopup("You shot ".$p->getName()."!!!!");
+}
+}
+}
+}
