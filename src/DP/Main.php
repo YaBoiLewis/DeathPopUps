3fab7b97889d69e459e35b95334766cb35ed85f4
Pizzahaet <Graysteven09.sg@gmail.com>
2017-08-22 11:13:54 -0400

    fixes trash ass formating, I am not even going to try and fix the rest of the cancer!

--- src/DP/Main.php
+++ src/DP/Main.php
@@ -13,29 +13,26 @@ use pocketmine\event\entity\EntityDamageByChildEntityEvent;
 use pocketmine\event\player\PlayerDeathEvent;
 
 class Main extends PluginBase implements Listener{
+    public function onEnable(){
+        $this->getServer()->getLogger()->info(TextFormat::BLUE."[DeathPopups]Plugin Enabled!");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
 
-public function onEnable(){
-
-$this->getServer()->getLogger()->info(TextFormat::BLUE."[DeathPopups]Plugin Enabled!");
-$this->getServer()->getPluginManager()->registerEvents($this,$this);
-}
-
-public function onDeath(PlayerDeathEvent $ev){
-$p=$ev->getEntity();
-if($ev instance of EntityDamageByEntityEvent){
-$killer = $ev->getEntity()->getLastDamageCause()->getDamager();
-if($ev instanceof Player){
-foreach($this->getServer()->getOnlinePlayers() as $pl){
- $pl->sendTip($killer->getName()."-=-".$p->getName()); 
-}
-}
-}
-}
-if($ev instance of EntityDamageByChildEntityEvent){
-$killer = $ev->getEntity()->getLastDamageCause()->getDamager();
-if($ev instanceof Player){
-$killer->sendPopup("You sho".$p->getName()."!!!!");
-}
-}
-}
+    public function onDeath(PlayerDeathEvent $ev){
+        $p = $ev->getEntity();
+        if ($ev instanceof EntityDamageByEntityEvent) {
+            $killer = $ev->getEntity()->getLastDamageCause();
+        }
+        if ($ev instanceof Player) {
+            foreach ($this->getServer()->getOnlinePlayers() as $pl) {
+                $pl->sendTip($killer->getName() . "-=-" . $p->getName());
+            }
+        }
+        if ($ev instanceof EntityDamageByChildEntityEvent) {
+            $killer = $ev->getEntity()->getLastDamageCause();
+        }
+        if ($ev instanceof Player) {
+            $killer->sendPopup("You sho" . $p->getName() . "!!!!");
+        }
+    }
 }
